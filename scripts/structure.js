d37e293f5b085ac198712c1bde74ea8c6306570f
K1-RA <indrajeetgodse@gmail.com>
2021-10-13 13:01:38 +0200

    added gifs to Images folder

--- scripts/structure.js
+++ scripts/structure.js
@@ -1,47 +1,66 @@
 //Navigation functions
 
 function init() {
-    createLandingPage();
+  createLandingPage();
+  displaySummary();
 }
 
 function createLandingPage() {
-    //Insert HTML into the main that creates the landing page
+  //Insert HTML into the main that creates the landing page
 }
 
 function openHighscorePage() {
-    //Clear main and insert html for the highscore page
-
-    //Will call retrieveHighscores()
+  //Clear main and insert html for the highscore page
+  //Will call retrieveHighscores()
 }
 
-function retrieveHighscores () {
-    //Will be called by the OpenHighscorePage function
-    //Retrieve the first ten highscores for the user from the local storage 
-    //(scores should be stored ordered by lowest time to highest so retrieving first 10 will work)
-    //Insert relevant data from those top ten into the page as it is loaded (date, time, average reaction time)
+function retrieveHighscores() {
+  //Will be called by the OpenHighscorePage function
+  //Retrieve the first ten highscores for the user from the local storage
+  //(scores should be stored ordered by lowest time to highest so retrieving first 10 will work)
+  //Insert relevant data from those top ten into the page as it is loaded (date, time, average reaction time)
 }
 
-function launchGame () {
-    //Clear the main and insert the HTML for the white rabbit game
+function difficultyPage() {
+  //Clear the main and insert the HTML for the difficulty page
+  //Set difficulty and save it to session storage on click
+  //Call launchGamePage()
+}
+function launchGamePage() {
+  //Clear the main and insert the HTML for the white rabbit game
 }
 
-function displaySummary () {
-    //Clear the main and insert the HTML for the summary screen
-    //Retrieve the time for the last game from local storage
-
-    //Will call calculateAverageReaction()
-    //Will call displayCharacter()
+function displaySummary() {
+  document.getElementById("content").innerHTML = "";
+  document.getElementById("content").innerHTML = `
+    <div id="scoreQuote">
+        <h4>
+          I know what you are thinking, cause right now I'm thinking the same
+          thing.<br />
+          Actually, I've been thinking it ever since I got here: <br />
+          WHY OH WHY DIDN'T I TAKE THE BLUE PILL?!?!
+        </h4>
+      </div>
+      <div id="SCORE">
+        <h2>SCORE:</h2>
+        <h2>You are the Chosen One!</h2>
+        <embed src="./Gifs/look-neo.gif" You are the Chosen One! />
+      </div>`;
+  //Clear the main and insert the HTML for the summary screen
+  //Retrieve the time for the last game from local storage
+  //Will call calculateAverageReaction()
+  //Will call displayCharacter()
 }
 
-function calculateAverageReaction () {
-    //Will be called in the displaySummary function
-    //Retrieve the ten reaction times saved into sessionStorage/a variable during the last game
-    //Average the scores to give an average reaction time
+function calculateAverageReaction() {
+  //Will be called in the displaySummary function
+  //Retrieve the ten reaction times saved into sessionStorage/a variable during the last game
+  //Average the scores to give an average reaction time
 }
 
 const characters = []; //Fill this array with the Matrix chaaracters
 
-function displayCharacter () {
-    //Will be called in the displaySummary function
-    //Use an else if statement to check the game time against the thresholds we picked, then retrieve a picture from the characters array
+function displayCharacter() {
+  //Will be called in the displaySummary function
+  //Use an else if statement to check the game time against the thresholds we picked, then retrieve a picture from the characters array
 }
